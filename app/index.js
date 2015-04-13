7d34e5e046416be370c9e876fa072221f8669c80
Balard Cedric <cedric.balard@yocto.re>
2015-04-13 18:26:06 +0400

    Modification du fichier 'dependencies.json', ajout catégorie : 'required', 'optional', 'db', avec spécification des version si besoin. Ajout logger 'winston'

--- app/index.js
+++ app/index.js
@@ -1,44 +1,62 @@
 /**
  * Main file of the 'yocto-stack-generator'
  *
- * @author
+ * @author Cédric Balard
  */
 
 'use strict';
-var yeoman = require('yeoman-generator');
-var mkdirp = require('mkdirp');
-var _ = require('lodash');
+var yeoman  = require('yeoman-generator');
+var mkdirp  = require('mkdirp');
+var _       = require('lodash');
+var logger  = require('winston');
 
 var npmDependenciesFile = require('./config/dependencies.json');
-console.log('List of Npm Dependencies : loaded ');
+logger.info('List of Npm Dependencies : loaded ');
+
 
 
 module.exports= yeoman.generators.Base.extend({
 
     /**
      * Initializing, load .json config files
-     * @type {Object}
      */
-    initializing: {
-
-        getPathFoldrers: function(){
+    initializing : {
+        /**
+         * Read "./config/folders.json" and get all folders
+         */
+        getPathFoldrers : function() {
             this.pathFolders = require('./config/folders.json');
-            this.log('Folders list to create : loaded ');
+            logger.info('Folders list to create : loaded ');
         },
 
-        getNpmDepencies: function(){
-            this.listNpmDependenciesOptional = [];
+        /**
+         * Read "dependencies.json" and get all dependencies
+         */
+        getNpmDepencies : function() {
             this.listNpmDependencies = [];
-
-            _.forEach( npmDependenciesFile.dependencies, function(dep) {
-                if( dep.confirm === 'true' )
-                    this.listNpmDependenciesOptional.push( dep.name);
-                else
-                    this.listNpmDependencies.push(dep.name);
+            _.forEach( npmDependenciesFile.required, function(dep) {
+                this.listNpmDependencies.push(dep.dependencies);
             }, this );
         },
 
-        getDDConfig: function(){
+        /**
+         * Read "dependencies.json" and get all optional dependencies
+         */
+        getNpmDepenciesOptional : function() {
+            this.listNpmDependenciesOptional = [];
+            _.forEach( npmDependenciesFile.optional, function(dep) {
+                if (_.isString(dep.dependencies) )
+                    this.listNpmDependenciesOptional.push( dep.dependencies);
+                else
+                    this.listNpmDependenciesOptional.push(  _([dep.dependencies.name, dep.dependencies.version]).join("@"));
+
+            }, this );
+        },
+
+        /**
+         * Get different type of DB
+         */
+        getDBConfig : function() {
             this.dbConfigPossibles = [];
             _.forEach( npmDependenciesFile.db, function(db) {
                 this.dbConfigPossibles.push(db.type);
@@ -50,40 +68,53 @@ module.exports= yeoman.generators.Base.extend({
      * Handle user intereactions
      */
     prompting: {
-        promptInfoProject: function(){
-
+        /**
+         * Ask user about general project's info
+         */
+        promptInfoProject : function() {
             var done = this.async();
             var prompts = [{
-                name: 'appName',
-                message: 'What is your app\'s name ?',
-                validate: function(input){
-                    if( input !== ''){
+                name        : 'appName',
+                message     : 'What is your app\'s name ?',
+                validate    : function(input){
+                    if( _.isEmpty(input) )
+                        return false;
+                    else
                         return true;
-                     }
-                    return false;
                 }
             },
+
             {
-                name: 'appDescription',
-                message: 'What is your app\'s desription ?',
-                validate: function(input){
-                    if( input !== ''){
+                name        : 'appDescription',
+                message     : 'What is your app\'s desription ?',
+                validate    : function(input) {
+                    if( _.isEmpty(input) )
+                        return false;
+                    else
                         return true;
-                     }
-                    return false;
                 }
             },
+
             {
-                name: 'appVersion',
-                message: 'What is your app\'s version (format x.x.x) ?',
-                default : '0.0.0'
+                name        : 'appVersion',
+                message     : 'What is your app\'s version (format x.x.x) ?',
+                default     : '1.0.1',
+                validate    : function(input) {
+                    var reg = /^(?:(\d+)\.)?(?:(\d+)\.)?(\d+)$/;
+                    if( reg.exec(input))
+                        return true;
+                    else
+                        return false;
+                }
             },
+
             {
-                name: 'appIsPrivate',
-                type: 'list',
-                message: 'Your app\'s is private ?',
-                choices: ['true', 'false'],
+                name    : 'appIsPrivate',
+                type    : 'list',
+                message : 'Your app\'s is private ?',
+                choices : ['true', 'false'],
             }];
+
             this.prompt(prompts, function (props) {
                 this.appName = props.appName;
                 this.appDescription = props.appDescription;
@@ -93,32 +124,43 @@ module.exports= yeoman.generators.Base.extend({
             }.bind(this));
         },
 
+        /**
+         * Ask user about wich npm dependencie optional he want install
+         */
         promptNpmDependenciesOptional: function()
         {
-            var done = this.async();
-            var prompts = [{
-                type: 'checkbox',
-                name: 'npmDepOption',
-                message: 'Select which dependencies you want to install (click on "space" for select/unselect) :',
-                choices: this.listNpmDependenciesOptional
-            }];
-            this.prompt(prompts, function (props) {
-                _.forEach(props.npmDepOption, function(val){
-                    this.listNpmDependencies.push(val);
-                }, this);
-                done();
-            }.bind(this));
+            if( ! _.isEmpty(this.listNpmDependenciesOptional) )
+            {
+                var done = this.async();
+                var prompts = [{
+                    type    : 'checkbox',
+                    name    : 'npmDepOption',
+                    message : 'Select which dependencies you want to install (click on "space" for select/unselect) :',
+                    choices : this.listNpmDependenciesOptional
+                }];
+
+                this.prompt(prompts, function (props) {
+                    _.forEach(props.npmDepOption, function(val){
+                        this.listNpmDependencies.push(val);
+                    }, this);
+                    done();
+                }.bind(this));
+            }
         },
 
+        /**
+        * Ask user about wich type of database he want install
+         */
         promptDbChoices: function()
         {
             var done = this.async();
             var prompts = [{
-                type: 'list',
-                name: 'npmDbChoice',
-                message: 'Select which type of database you want to install :',
-                choices: this.dbConfigPossibles
+                type    : 'list',
+                name    : 'npmDbChoice',
+                message : 'Select which type of database you want to install :',
+                choices : this.dbConfigPossibles
             }];
+
             this.prompt(prompts, function (props) {
                     this.dbType=props.npmDbChoice;
                 done();
@@ -128,7 +170,6 @@ module.exports= yeoman.generators.Base.extend({
 
     /**
      * Handle configuration : make all directory ...
-     * @type {Object}
      */
     configuring: {
         /**
@@ -138,26 +179,20 @@ module.exports= yeoman.generators.Base.extend({
             _.forEach( this.pathFolders.folders, function(value)
             {
                 mkdirp(value.path);
-                this.log('New folder created' , value.path);
-            },this);
+                logger.info( _(['New folder created :', value.path]).join(" ") );
+            });
         },
 
         /**
          * Handle DB dependencies in correlation with the dbType choosen
+         * Add specifics dependencies to install
          */
-        setDependenciesDB: function(){
+        setDependenciesDB : function() {
             _.forEach( npmDependenciesFile.db, function(db) {
                 if(this.dbType === db.type )
                 {
-                    this.log ('ok dbtype found ');
                     _.forEach(db.dependencies, function(dep){
-                            this.log('dep ' + dep);
-                            // if (typeof myVar == 'string' || myVar instanceof String)
-                            // // it's a string
-                            // else
-                            //
-                            this.listNpmDependencies.push(dep);
-
+                        this.listNpmDependencies.push(dep);
                     } ,this )   ;
                 }
             }, this );
@@ -167,14 +202,15 @@ module.exports= yeoman.generators.Base.extend({
     /**
      * Generate specifics files : package.json, routes...
      */
-    writing:{
-        copyMainFile: function(){
+    writing : {
+        copyMainFile : function() {
             var context = {
-                siteName: this.appName,
-                siteDescription: this.appDescription,
-                siteVersion: this.appVersion,
-                siteIsPrivate: this.appIsPrivate
+                siteName            : this.appName,
+                siteDescription     : this.appDescription,
+                siteVersion         : this.appVersion,
+                siteIsPrivate       : this.appIsPrivate
             };
+
             this.copy('_gruntfile.js', 'Gruntfile.js');
             this.template('_package.json', 'package.json', context);
         }
@@ -184,24 +220,25 @@ module.exports= yeoman.generators.Base.extend({
      * Install all dependencies : npm and Bower
      */
     install:{
+        /**
+         * Install all npm dependencies
+         */
         installNpmDependencies: function() {
-            this.log('install npm dependencies');
+            this.log('Install npm dependencies');
             _.forEach(this.listNpmDependencies, function(dep)
             {
-                if (typeof dep == 'string' || dep instanceof String)
+                if (_.isString(dep))
                 {
                     this.npmInstall([dep], { 'save': true });
-                    this.log('package ', dep , ' installed');
+                    logger.info('Package installed : ' + dep );
                 }
                 else
                 {
-                    this.npmInstall([dep], { 'save': true });
-                    this.log('package ' + dep.name + ', version : ' + dep.version + ' was installed');
+                    this.npmInstall( _([dep.name,dep.version]).join("@") );
+                    logger.info(  _(['Package installed : ', dep.name, dep.version]).join("@") );
                 }
-
             }, this);
         },
-
         installBowerDependencies: function(){
         }
     }
